ba9e4c2f39d4aa7f50e1e50a540d3d27bcaf2bf2
symphony for <hekd@hsmap.cn>
2021-01-25 11:14:59 +0800

    第一次

---
+++ pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<view class="">
+		起飞
+	</view>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				
+			}
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+</style>
